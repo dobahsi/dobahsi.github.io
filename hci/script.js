ea79437479570c19e44c613bb4df0cc87306ec48
dobahsi <hsi.en07@nycu.edu.tw>
2023-05-29 18:40:02 +0800

    0

--- hci/script.js
+++ hci/script.js
@@ -4,3 +4,24 @@ console.log(nav);
 function menuOpen() {
     nav.classList.toggle('displaynone');
 }
+
+function toggleExtendUl(e) {
+    if (e.target.localName == 'box-icon') {
+        var ul = e.target.parentElement.parentElement.querySelector('.nav-extend-ul');
+
+    } else {
+        var ul = e.target.parentElement.querySelector('.nav-extend-ul');
+    }
+
+    var icon = ul.parentElement.querySelector('box-icon')
+
+    
+    if (ul.classList.contains('displaynone')) {
+        ul.classList.remove('displaynone')
+        icon.classList.add('rotate180');
+    } else {
+        ul.classList.add('displaynone')
+        icon.classList.remove('rotate180');
+    }
+    
+}
