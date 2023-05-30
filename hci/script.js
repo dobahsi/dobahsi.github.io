0d535f2f035f197fb847366e7267cc8e35365064
dobahsi <91683644+dobahsi@users.noreply.github.com>
2023-05-30 20:26:42 +0800

    Revert "colorChanges"

--- hci/script.js
+++ hci/script.js
@@ -10,7 +10,6 @@ function toggleMenu() {
 }
 
 function toggleExtendUl(e) {
-    
     if (e.target.localName == 'box-icon') {
         var ul = e.target.parentElement.parentElement.querySelector('.nav-extend-ul');
 
@@ -18,7 +17,7 @@ function toggleExtendUl(e) {
         var ul = e.target.parentElement.querySelector('.nav-extend-ul');
     }
 
-    var icon = ul.parentElement.querySelector('box-icon');
+    var icon = ul.parentElement.querySelector('box-icon')
 
     
     if (ul.classList.contains('displaynone')) {
@@ -27,6 +26,6 @@ function toggleExtendUl(e) {
     } else {
         ul.classList.add('displaynone')
         icon.classList.remove('rotate180');
-    }   
+    }
     
 }
